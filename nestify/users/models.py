ecd3c451ad9086df89df4a4f0e8be3de1c0561c0
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-03-23 21:16:02 +0200

    adding users urls, views and models

--- nestify/users/models.py
+++ nestify/users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    USER_TYPE_CHOICES = (
+        ('property_owner', 'Property Owner'),
+        ('admin', 'Admin'),
+    )
+    usertype = models.CharField(max_length=20, choices=USER_TYPE_CHOICES, default='property_owner')
--- nestify/users/views.py
+++ nestify/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics, permissions
+from .serializers import UserSerializer
+from .models import User
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
