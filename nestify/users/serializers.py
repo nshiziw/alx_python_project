74cdb53c361159ee431e95e6c0c50a287bcfc640
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-03-24 11:32:03 +0200

    fixing: inserting last_name, first_name, hashedPassword in the database

--- nestify/users/serializers.py
+++ nestify/users/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)  # Ensures password is not returned in responses
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'usertype']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'usertype', 'password']  # Added first_name, last_name, password
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')  # Extract password before creating the user
+        user = User.objects.create(**validated_data)  # Create user with other validated data
+        user.set_password(password)  # Hash the password before saving it
+        user.save()  # Save the user
+        return user
